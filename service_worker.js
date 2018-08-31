83f36da5a1168018c6a2f9c0985e98650d9dd68f
Christian Helbling <helbling@search.ch>
2018-08-31 15:45:29 +0200

    delete old caches on update

--- service_worker.js
+++ service_worker.js
@@ -1,8 +1,10 @@
 importScripts('/cache-polyfill.js');
 
+var cacheName = 'passist-v1';;
+
 self.addEventListener('install', function(e) {
 	e.waitUntil(
-		caches.open('passist-v1').then(function(cache) {
+		caches.open(cacheName).then(function(cache) {
 			return cache.addAll([
 				'/',
 				'/styles.css',
@@ -19,10 +21,23 @@ self.addEventListener('install', function(e) {
 	);
 });
 
+// delete old caches on activate
+this.addEventListener('activate', function(event) {
+	event.waitUntil(
+		caches.keys().then(function(keyList) {
+			return Promise.all(keyList.map(function(key) {
+				if (key != cacheName) {
+					return caches.delete(key);
+				}
+			}));
+		})
+	);
+});
+
 // https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook/#stale-while-revalidate
 self.addEventListener('fetch', function(event) {
 	event.respondWith(
-		caches.open('passist-v1').then(function(cache) {
+		caches.open(cacheName).then(function(cache) {
 			return cache.match(event.request).then(function(response) {
 				var fetchPromise = fetch(event.request).then(function(networkResponse) {
 					if (event.request.url.match(/^http/))
