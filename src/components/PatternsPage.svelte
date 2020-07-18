92129afb1915676648e408e5f15e23a7839280e1
Christian Helbling <helbling@search.ch>
2020-07-18 14:24:25 +0200

    make patterns page more usable on mobile by putting links into menu buttons

--- src/components/PatternsPage.svelte
+++ src/components/PatternsPage.svelte
@@ -3,23 +3,23 @@
 </script>
 
 <style>
-	ul{ padding-left:0; column-width:20em }
-	li { list-style-type:none }
-	li .siteswap { display:inline-block; width:5.5em ; vertical-align:top; overflow-wrap:break-word}
-	li .name { display:inline-block; text-overflow:ellipsis; width:15em }
-	a:hover span { text-decoration:underline }
+	li { text-overflow:ellipsis; overflow:hidden }
+	li .siteswap { width:5.5em; vertical-align:top; overflow:hidden; text-overflow:ellipsis }
+	span { display:inline-block; color:#212529 }
 </style>
 <h1>Well-known siteswaps</h1>
 
 <p><small>© Christian Kästner, <a href='https://github.com/ckaestne/CompatSiteswaps/blob/master/named-siteswaps.txt'>named-siteswaps.txt</a></p>
 
-<ul>
+<div class=pure-menu>
+<ul class="pure-menu-list pure-g">
 	{#each knownSiteswaps as s}
-		<li>
-			<a href="/siteswap/{s[0]}?nJugglers=2">
+		<li class="pure-menu-item pure-u-1 pure-u-md-1-2 pure-u-lg-1-3">
+			<a class=pure-menu-link href="/siteswap/{s[0]}?nJugglers=2">
 				<span class=siteswap>{s[0]}</span>
 				<span class=name>{s[1]}</span>
 			</a>
 		</li>
 	{/each}
 </ul>
+</div>
