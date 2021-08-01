6eff9ddb8f660b76a88bd8112186867b8252bacd
Christian Helbling <helbling@search.ch>
2021-08-01 12:17:39 +0200

    simplify - adapter-node api is better now

--- src/routes/api/examples/[example].jif.js
+++ src/routes/api/examples/[example].jif.js
@@ -5,7 +5,7 @@ export async function get({ params }) {
 	const strippedExample = example.replace(/[^a-zA-Z0-9._-]/, ''); // better be save to avoid data leaks
 	try {
 		return {
-			body: (await promises.readFile(`static/api/_examples/${strippedExample}.jif`)).toString(),
+			body: await promises.readFile(`static/api/_examples/${strippedExample}.jif`),
 		};
 	} catch {
 		return {
--- src/routes/images/[image].js
+++ src/routes/images/[image].js
@@ -5,10 +5,7 @@ export async function get({ params }) {
 	const strippedImage = image.replace(/[^a-zA-Z0-9._-]/, ''); // better be save to avoid data leaks
 	try {
 		return {
-			headers: {
-				'content-type': 'application/octet-stream', // will be replaced by src/hooks.js
-			},
-			body: (await promises.readFile(`static/_images/${strippedImage}`)),
+			body: await promises.readFile(`static/_images/${strippedImage}`),
 		};
 	} catch {
 		return {
