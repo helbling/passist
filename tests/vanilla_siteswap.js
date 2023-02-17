15452741455e663e50be68bbf0ccf56678960962
Christian Helbling <helbling@search.ch>
2023-02-17 14:07:51 +0100

    test jif generation as well

--- tests/vanilla_siteswap.js
+++ tests/vanilla_siteswap.js
@@ -3,6 +3,22 @@ import * as assert from 'uvu/assert';
 import Siteswap from '../src/lib/siteswap.mjs';
 import { patterns } from '../src/lib/patterns.mjs'
 
+function defaultLimbs(n) // TODO: avoid copy-pasta
+{
+	const result = [];
+	for (let i = 0; i < 2 * n; i++) {
+		const juggler = i % n;
+
+		// alternating right and left for an odd number of jugglers makes the patterns more symmetric (Co Stuifbergen)
+		const right = (n % 2) ? !(i % 2) : i < n;
+		result.push({
+			juggler,
+			type: (right ? 'right' : 'left') + ' hand',
+		});
+	}
+	return result;
+}
+
 for (const pattern of patterns) {
 	if (pattern.type != 'vanilla_siteswap')
 		continue;
@@ -12,6 +28,27 @@ for (const pattern of patterns) {
 	test(notation + ' expecting valid siteswap', () => {
 		assert.ok(siteswap.isValid());
 	});
+
+	for (let nJugglers = 1; nJugglers <= 3; nJugglers++) {
+		const jugglers = [];
+		for (let i = 0; i < nJugglers; i++)
+			jugglers.push({ name: String.fromCharCode(65 + i)});
+
+		let exception = false;
+		try {
+			siteswap.toJif({
+				jugglers,
+				limbs: defaultLimbs(nJugglers),
+			});
+		} catch(e) {
+			console.log(notation, e);
+			exception = true;
+		}
+
+		test(notation + ` no exception when generating jif for ${nJugglers} jugglers`, () => {
+			assert.ok(!exception);
+		});
+	}
 }
 
 test.run();
