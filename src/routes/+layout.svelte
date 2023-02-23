b68dc0cc3bb169b117035f3d97d4529c2a0d7523
Christian Helbling <helbling@search.ch>
2023-02-23 21:36:41 +0100

    active siteswap tab on /symmetric-siteswap

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 	$: {
 		const pathname = $page?.url?.pathname || '';
 		segment = '/' + pathname.split('/')[1];
-		if (segment == '/siteswap' || segment == '/extended-siteswap')
+		if (segment.match('/(|symmetric-|extended-)siteswap'))
 			segment = '/pattern';
 	}
 
