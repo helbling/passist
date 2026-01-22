19807bb4bf30af816d095cbe018af62fff15418d
Christian Helbling <helbling@search.ch>
2026-01-22 17:49:58 +0100

    add missing </a> tag

--- src/routes/api/+page.svelte
+++ src/routes/api/+page.svelte
@@ -3,13 +3,13 @@
 <h2>Demos</h2>
 
 <ul>
-	<li><a href="/api/causal-diagram-widget-demo.html">Causal Diagram Widget Demo</li>
+	<li><a href="/api/causal-diagram-widget-demo.html">Causal Diagram Widget Demo</a></li>
 	<li><a href="/api/animation-widget-demo.html">Animation Widget Demo</a></li>
 </ul>
 
 
 <h2>JS modules</h2>
 <ul>
-	<li><a href="/api/causal-diagram-widget-standalone.mjs">Causal Diagram Widget</li>
+	<li><a href="/api/causal-diagram-widget-standalone.mjs">Causal Diagram Widget</a></li>
 	<li><a href="/api/animation-widget-standalone.mjs">Animation Widget</a></li>
 </ul>
